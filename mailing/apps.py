e141ec2d3be0598a15aeabe2ee770d3fac1f539f
estafeeva <stafeeva.ea@mail.ru>
2024-07-14 18:12:21 +0300

    Make a mailing list script that works both from the command line and on a schedule. Add configuration settings to run the task periodically if necessary.

--- mailing/apps.py
+++ mailing/apps.py
@@ -1,11 +1,20 @@
+from time import sleep
+
 from django.apps import AppConfig
+
+from config.settings import RUN_SCHEDULER
 
 
 class MailingConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailing"
 
-    """def ready(self):
-         from mailing.модуль_с_задачей import функция_старта 
-         sleep(2)
-         функция_старта()"""
+    def ready(self):
+        print(RUN_SCHEDULER)
+        if RUN_SCHEDULER:
+            from mailing.services import start_scheduler
+            sleep(2)
+            start_scheduler()
+            print('scheduler started')
+        else:
+            print('scheduler not started')
--- mailing/services.py
+++ mailing/services.py
@@ -1,2 +1,16 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from mailing.models import Client, MailingSettings, MailingAttempt
+
+
 def mailing_sending():
     print("Рассылка отправилась")
+
+
+def start_scheduler():
+    scheduler = BackgroundScheduler()
+
+    if not scheduler.get_jobs():
+        scheduler.add_job(mailing_sending, 'interval', seconds=20)
+
+    if not scheduler.running:
+        scheduler.start()
